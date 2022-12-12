fe12db09c2f4e3f20b0a55bf8a71cfaba1555913
muhammadabdissalam <mhdabdissalam@gmail.com>
2022-12-12 18:47:04 +0700

    update content

--- scripts/create-file.js
+++ scripts/create-file.js
@@ -1,9 +1,9 @@
 const fse = require('fs-extra');
 fse
   .outputFile(
-    'pages/institution/detail-institution.mdx',
-    '# Detail Institution \n [Edit this page in github](https://github.com/muhammadabdissalam/hello-nextra/blob/main/pages/institution/detail-institution.mdx)',
+    'pages/appendix/detail-appendix.mdx',
+    '# Detail appendix \n [Edit this page in github](https://github.com/muhammadabdissalam/hello-nextra/blob/main/pages/appendix/detail-appendix.mdx)',
   )
   .then(() => {
-    console.log('created detail-institution');
+    console.log('created detail-appendix');
   });
