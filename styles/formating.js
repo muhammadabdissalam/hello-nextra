df4131dafc82135e8a049155fb6b959b20b5cc08
muhammadabdissalam <mhdabdissalam@gmail.com>
2022-12-13 13:31:20 +0700

    update syntax highlighting

---
+++ styles/formating.js
@@ -0,0 +1,35 @@
+{
+  ('entry');
+  [
+    {
+      fullUrl:
+        'https://api-satusehat-dev.dto.kemkes.go.id/fhir-r4/v1/Patient/100000030009',
+      resource: {
+        _birthDate: {
+          extension: [
+            {
+              url: 'http://hl7.org/fhir/StructureDefinition/patient-birthTime',
+              valueDateTime:
+                '1944-11-17T15:39:00+07:00',
+            },
+          ],
+        },
+      },
+    },
+    {
+      fullUrl:
+        'https://api-satusehat-dev.dto.kemkes.go.id/fhir-r4/v1/Patient/100000030009',
+      resource: {
+        _birthDate: {
+          extension: [
+            {
+              url: 'http://hl7.org/fhir/StructureDefinition/patient-birthTime',
+              valueDateTime:
+                '1944-11-17T15:39:00+07:00',
+            },
+          ],
+        },
+      },
+    },
+  ];
+}
