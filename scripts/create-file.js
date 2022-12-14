b473e5eed05c36c8707a50d2ce12f72ea4b84e49
muhammadabdissalam <mhdabdissalam@gmail.com>
2022-12-14 13:48:08 +0700

    ignore _meta.json

--- scripts/create-file.js
+++ scripts/create-file.js
@@ -1,9 +1,11 @@
 const fse = require('fs-extra');
 fse
   .outputFile(
-    'pages/appendix/detail-appendix.mdx',
-    '# Detail appendix \n [Edit this page in github](https://github.com/muhammadabdissalam/hello-nextra/blob/main/pages/appendix/detail-appendix.mdx)',
+    'pages/appendix/review-appendix.mdx',
+    '# review appendix \n [Edit this page in github](https://github.com/muhammadabdissalam/hello-nextra/blob/main/pages/appendix/review-appendix.mdx)',
   )
   .then(() => {
-    console.log('created detail-appendix');
+    console.log(
+      'created review-appendix',
+    );
   });
